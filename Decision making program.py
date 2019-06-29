89e01a137c6e65cb5a1361edaa34b6898529a529
Maxwb7 <Maxwb7@gmail.com>
2019-06-29 11:37:42 -0700

    Add files via upload

---
+++ Decision making program.py
@@ -0,0 +1,28 @@
+# a decision helper program
+
+
+print('will this decision matter in 5 year?")
+
+input("yes, no, maybe")
+
+if input = ('yes'):
+    print("ok, let's list the pros and cons")
+    #here run the pros vs cons function
+    #first enter in pros
+    #2nd enter the cons
+    #3rd print pros vs cons in a visual style
+    #then we say ok, did that help, if it did then we are done if not we can move on
+    
+    
+elif input = ("no"):
+    print("ok, let's flip a coin.")
+    #here we will run a simple coin flipper function.
+    #then we as "did that help?" Y/N if yes done, if no move on to questions.
+    
+elif input = ("maybe"):
+    print("ok, let's as some peliminary questions")
+    #here we execute the peliminary questions function.
+    #this function will either bring us back to coin flip or direct us to pros and cons.
+    
+    
+    
